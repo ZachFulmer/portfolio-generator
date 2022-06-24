95d397ce60e5b304bfb05377ff20312ca4cbe099
Zach Fulmer <Zachfulmer@earthlink.net>
2022-06-23 20:38:56 -0500

    code refactor for multiple project data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
-
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () =>
 {
@@ -145,17 +146,14 @@ const promptProject = portfolioData =>
 
 promptUser()
     .then(promptProject)
-    .then(portfolioData => console.log(portfolioData));
-//const fs = require('fs');
-/*
-const generatePage = require('./src/page-template.js');
-const pageHTML = generatePage(name, github);
+    .then(portfolioData => 
+        {
+            const pageHTML = generatePage(portfolioData);
 
+             fs.writeFile('./index.html', pageHTML, err => {
+               if (err) throw new Error(err);
 
-fs.writeFile('./index.html', pageHTML, err => 
-{
-    if(err) throw err;
-
-    console.log('Portfolio Complete! Check out index.html to see the output!');
-});
-*/
+               console.log('Page created! Check out index.html in this directory to see it!');
+             });
+   
+        });
